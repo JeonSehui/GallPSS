b948e2699a7a3753efee855dbecd3a9cf4e4ff27
Jayce <chickang9@gmail.com>
2020-10-26 17:02:10 +0900

    Edit Back view for connect url

--- boards/urls.py
+++ boards/urls.py
@@ -3,4 +3,7 @@ from boards import views as board_views
 
 app_name = "boards"
 
-urlpatterns = [path("main", board_views.Category.as_view(), name="main")]
+urlpatterns = [path("main", board_views.Category.as_view(), name="main"),
+                path("boardid/<int:id>", board_views.GetBoardItem.as_view(), name="getItemDetail"),
+                path("itemid/<int:id>", board_views.GetBoardItem.as_view(), name="getItemDetail"),
+                path("create/<int:id>", board_views.GetBoardItem.as_view(), name="getItemDetail")]
